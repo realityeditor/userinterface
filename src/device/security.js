ceb3e26570d3713f626ac0a67d2e17c3ca9dad11
Benjamin Reynolds <reynoldsben94@gmail.com>
2017-02-07 12:16:18 -0500

    touchID authentication logic but no UI yet

---
+++ src/device/security.js
@@ -0,0 +1,74 @@
+/**
+ * @preserve
+ *
+ *                                      .,,,;;,'''..
+ *                                  .'','...     ..',,,.
+ *                                .,,,,,,',,',;;:;,.  .,l,
+ *                               .,',.     ...     ,;,   :l.
+ *                              ':;.    .'.:do;;.    .c   ol;'.
+ *       ';;'                   ;.;    ', .dkl';,    .c   :; .'.',::,,'''.
+ *      ',,;;;,.                ; .,'     .'''.    .'.   .d;''.''''.
+ *     .oxddl;::,,.             ',  .'''.   .... .'.   ,:;..
+ *      .'cOX0OOkdoc.            .,'.   .. .....     'lc.
+ *     .:;,,::co0XOko'              ....''..'.'''''''.
+ *     .dxk0KKdc:cdOXKl............. .. ..,c....
+ *      .',lxOOxl:'':xkl,',......'....    ,'.
+ *           .';:oo:...                        .
+ *                .cd,      ╔═╗┌┬┐┬┌┬┐┌─┐┬─┐    .
+ *                  .l;     ║╣  │││ │ │ │├┬┘    '
+ *                    'l.   ╚═╝─┴┘┴ ┴ └─┘┴└─   '.
+ *                     .o.                   ...
+ *                      .''''','.;:''.........
+ *                           .'  .l
+ *                          .:.   l'
+ *                         .:.    .l.
+ *                        .x:      :k;,.
+ *                        cxlc;    cdc,,;;.
+ *                       'l :..   .c  ,
+ *                       o.
+ *                      .,
+ *
+ *      ╦═╗┌─┐┌─┐┬  ┬┌┬┐┬ ┬  ╔═╗┌┬┐┬┌┬┐┌─┐┬─┐  ╔═╗┬─┐┌─┐ ┬┌─┐┌─┐┌┬┐
+ *      ╠╦╝├┤ ├─┤│  │ │ └┬┘  ║╣  │││ │ │ │├┬┘  ╠═╝├┬┘│ │ │├┤ │   │
+ *      ╩╚═└─┘┴ ┴┴─┘┴ ┴  ┴   ╚═╝─┴┘┴ ┴ └─┘┴└─  ╩  ┴└─└─┘└┘└─┘└─┘ ┴
+ *
+ *
+ * Created by Valentin on 10/22/14.
+ *
+ * Copyright (c) 2015 Valentin Heun
+ * Modified by Valentin Heun 2014, 2015, 2016, 2017
+ * Modified by Benjamin Reynolds 2016, 2017
+ * Modified by James Hobin 2016, 2017
+ *
+ * All ascii characters above must be included in any redistribution.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * Created by benreynolds on 2/7/17.
+ */
+
+createNameSpace("realityEditor.device.security");
+
+realityEditor.device.security.authenticateSessionForUser = function(encryptedId) {
+    console.log("js did run");
+    console.log("authenticating with userId: " + encryptedId);
+    if (encryptedId === null) {
+        console.log("authentication failed");
+        if (document.getElementById("adminModeSwitch").checked) {
+            document.getElementById("adminModeSwitch").click(); //.checked = false;            
+        }
+        globalStates.adminMode = false;
+        globalStates.authenticatedUser = null;
+        console.log("Is admin mode on now? " + globalStates.adminMode);
+    } else {
+        console.log("success");
+        globalStates.adminMode = true;
+        globalStates.authenticatedUser = encryptedId;
+        console.log("Is admin mode on now? " + globalStates.adminMode);
+    }
+    //console.log(objectExp);
+};
